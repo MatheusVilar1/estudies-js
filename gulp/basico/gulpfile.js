1a06ce1d7816f49815337101de3ac59b984bfff6
MatheusVilar1 <anmavigu13@gmail.com>
2022-05-16 19:28:59 -0300

    executando tarefas em paralelo

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,10 +1,26 @@
 const gulp = require('gulp')
-// const {series} = require('gulp')
-const series = gulp.series
+const {series,parallel} = require('gulp')
+//const series = gulp.series
 
+const antes1 = cb =>{
+    console.log("Tarefa 1")
+    return cb()
+}
+
+const antes2 = cb =>{
+    console.log("Tarefa 2")
+    return cb()
+}
 function copiar(cb){
     console.log('Tarefa de copiar!!')
     return cb()
 }
+const fim = cb =>{
+    console.log("tarefa fim")
+    return cb()
+}
 
-module.exports.default = series(copiar)
+module.exports.default = series(
+    parallel(antes1,antes2),
+    copiar,
+    fim)
