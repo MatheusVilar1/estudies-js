0697f41637c31e5caeee3a2e17a14e5e39832b1c
MatheusVilar1 <anmavigu13@gmail.com>
2022-05-18 14:00:39 -0300

    carregando arquivos sass

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,6 +1,7 @@
 // const Pessoa = require('./pessoa')
 import Pessoa from './pessoa'
 import './modulos/moduloA.js'
+import './assets'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,7 +1,27 @@
 const webpack = require('webpack')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
     mode:'development', //production(padrão) => gera uma unica linha, sem comments
-    entry:'./src/principal.js'
+    entry:'./src/principal.js',
+    output:{
+        filename:'principal.js',
+        path:__dirname +'/public'
+
+    },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename:'estilo.css'})
+    ],
+    module:{
+        rules:[{
+            test: /\.s?[ac]ss$/,
+            use:[
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', //adiciona o css a DOM(coloca a tag <style>)
+                'css-loader', // interpreta @import, url()
+                'sass-loader',
+            ]
+        }]
+    }
 
 }
